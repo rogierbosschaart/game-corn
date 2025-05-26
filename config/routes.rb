db947620707e57120664ecf291a486e05a6103fa
kazybek nurlanbek <kazybeknurlanbek@gmail.com>
2025-05-26 16:10:49 +0200

    updeted routes for offers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "offers#home"
+  resources :offers, only: [:show, :new, :create, :edit, :update, :destroy]
+
 end
