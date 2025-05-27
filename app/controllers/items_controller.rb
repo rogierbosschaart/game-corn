2722430697bcc8468252968b16dcd562d2b0d4b5
Tymur Topchu <timur.han98@gmail.com>
2025-05-27 14:35:25 +0200

    image create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,8 +9,26 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @user = current_user
+    @item.user_id = @user.id
+    if @item.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
+
+  def item_params
+    params.require(:item).permit(:title, :genre, :platform, :poster_url)
+  end
 
   def set_item
     @item = Item.find(params[:id])
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :offers
+
+  validates :title, :genre, :platform, :poster_url, presence: true
+  validates :title, uniqueness: { scope: :user, message: "you already add this game" }
 end
