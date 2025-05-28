d3c37f6a38cc4d84aba66e7bd3ea99dcc82bb049
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-05-28 11:56:03 +0200

    heroku setup, seedfile changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
 require "open-uri"
-
 puts "Cleaning up database..."
 Offer.destroy_all
 Item.destroy_all
 User.destroy_all
 puts "Database cleaned!"
-
 puts "Creating users..."
 # Create 5 sample users
 users = []
@@ -13,13 +11,10 @@ users << User.create!(username: "Tymur", city: "Amersfoort", email: "tymur@mahor
 users << User.create!(username: "Kazybek", city: "Amsterdan", email: "kazybek@dam.com", password: "123456")
 users << User.create!(username: "Rogier", city: "Amsterdan", email: "rogier@amster.com", password: "123456")
 puts "#{users.count} users created!"
-
 puts "Creating items..."
 # Define some common platforms and genres for items
 platforms = %w[PC XBOX PlayStation Nintendo Sega]
 genres = %w[RPG Shooter Strategy Racing Slasher Horror Stealth Platformer Action]
-# file = URI.parse("").open
-
 items = []
 users.each_with_index do |user, user_index|
   rand(3..7).times do |item_index| # Each user creates between 3 and 7 items
@@ -28,7 +23,6 @@ users.each_with_index do |user, user_index|
     # while Item.exists?(title: title) # This check is less necessary without Faker's randomness
     #   title = "Game Title #{rand(1000..9999)}"
     # end
-
     item = Item.new(
       title: title,
       platform: platforms.sample,
@@ -36,18 +30,13 @@ users.each_with_index do |user, user_index|
       description: "This is a sample description for #{title}. It's a great game!",
       user: user
     )
-
-  #   item.photo.attach(io: file, filename: "#{item.title}.jpg", content_type: "image/jpg")
-
-
-  # Simple placeholder image
-      item.save
-      items << item
-      puts "Created item: '#{item.title}' by #{user.username}"
+ # Simple placeholder image
+    item.save
+    items << item
+    puts "Created item: '#{item.title}' by #{user.username}"
   end
 end
 puts "#{items.count} items created!"
-
 puts "Creating offers..."
 # Create some offers
 # Ensure offers are between different users and for existing items
@@ -55,14 +44,11 @@ offers_count = 0
 15.times do |i| # Create 15 sample offers
   offering_user = users.sample
   item_to_offer = items.sample
-
   # Ensure the offering user is not the owner of the item
   next if offering_user == item_to_offer.user
-
   # Generate dates manually
   start_date = Date.today + (10 + i).days
   end_date = start_date + rand(7..30).days
-
   offer = Offer.create!(
     item: item_to_offer,
     user: offering_user,
@@ -74,5 +60,4 @@ offers_count = 0
   puts "Created offer for '#{item_to_offer.title}' by #{offering_user.username}"
 end
 puts "#{offers_count} offers created!"
-
 puts "Seeding complete!"
