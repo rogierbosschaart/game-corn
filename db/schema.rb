53571b4eaa558edb0d3b3088a5877a55db8a4f7e
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-05-30 14:45:17 +0200

    changed button

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_05_29_133110) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_05_30_104308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
